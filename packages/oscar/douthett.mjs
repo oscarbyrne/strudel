f8413e034fa2c1858e215819afdeb610295748e7
Oscar Byrne <oscar.byrne@gmail.com>
2024-01-21 16:01:31 -0800

    clean up FiPS code

--- packages/oscar/douthett.mjs
+++ packages/oscar/douthett.mjs
@@ -1,49 +1,38 @@
 import { listRange } from '@strudel.cycles/core';
 import { vectorAdd } from './util.mjs';
 
-export function maximallyEvenSet(c, d, m) {
-  return listRange(0, d - 1).map((k) => Math.floor((k * c + m) / d));
+export function J(c, d, m) {
+  return (...k) => k.map((ki) => Math.floor((ki * c + m) / d))
 }
 export class FiPS {
   constructor(...c) {
     this.c = c;
-    this.m = new Array(c.length - 1).fill(0);
+    this._m = new Array(c.length - 1).fill(0);
   }
-  withM(...m) {
-    let f = new FiPS(...this.c);
-    f.m = m;
-    return f;
-  }
-  j(pcs = listRange(0, 11)) {
-    for (let n = 0; n < this.c.length - 1; n++) {
-      pcs = pcs.map((k) => maximallyEvenSet(this.c[n + 1], this.c[n], this.m[n]).at(k % this.c[n]));
+  set m(m) {
+    console.log('New m: ' + m);
+    if (!Array.isArray(m) || m.length != this.c.length -1) {
+      throw new Error('Not a valid m: ' + m);
     }
-    return pcs;
+    this._m = m;
+  }
+  get m() {
+    return this._m;
+  }
+  J(beacon = null) {
+    let k = beacon || listRange(0, this.c[0] - 1);
+    for (var n = 0; n < this.c.length - 1; n++) {
+      k = J(this.c[n + 1], this.c[n], this.m[n])(...k);
+    }
+    return k;
   }
   d(...dm) {
     this.m = vectorAdd(this.m, dm);
-    console.log('New m: ' + this.m);
   }
-  dd(...ds) {
-    ds = ds.map(Math.sign);
-    let edge = new Array(this.m.length);
-    for (var n = 0; n < this.m.length; n++) {
-      edge[n] = this.boundary(n, ds[n]);
+  dd(...dm) {
+    const j = this.J();
+    while (this.J().every((k, n) => k == j[n])) {
+      this.d(...dm);
     }
-    this.d(...vectorAdd(edge, ds));
-  }
-  boundary(n, sign) {
-    sign = Math.sign(sign);
-    if (sign == 0) {
-      return 0;
-    }
-    const j = this.j();
-    let k = j;
-    let d = 0;
-    while (j.every((v, i) => v == k[i])) {
-      d += sign;
-      k = this.withM(...Object.assign([], this.m, { [n]: this.m[n] + d })).j();
-    }
-    return d - 1;
   }
 }
